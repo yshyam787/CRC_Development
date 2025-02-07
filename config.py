18921927c0dfb06399f0e2c3b1f69e5a3e4d3bc9
tutorial <yshyam787@gmail.com>
2025-02-07 19:38:15 +0100

    feat: added main_file.ipynb file and removed main.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #Root directory where the data folders and files are stored. 
-ROOT_DIRECTORY = "/Users/shyamyadav/Shyam/Git/OtherThanUniGoet/CRC_Development" #Change this as needed. 
+ROOT_DIRECTORY = "/Users/shyamyadav/Shyam/Git/OtherThanUniGoet/CRC_Development" #Change this as needed. Absolute path is needed. 
 
 #File_Configuration JSON file name
 CONFIGURATION_JSON = "file_configuration.json"
